6800f3822e4d21a5dcff7e8105f12414d2c6818f
JulianeSchulz <juliane.schulz@uni-weimar.de>
2016-04-25 15:23:46 +0200

    Aufagbe 1 fertig

---
+++ build/source/CMakeFiles/mltokm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/mltokm.dir/mltokm.cpp.o"
+  "../mltokm.pdb"
+  "../mltokm"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/mltokm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
